600d3173776729d544cf075aa4b4a8b7999a6636
Inventedforms <artifion@gmail.com>
2015-04-26 11:10:36 -0700

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,11 +1,17 @@
 import javax.swing.JFrame;
-
+import java.awt.*;
 
 public class Tester {
 	public static void main(String[] args)
 	{
+		JFrame frame = new JFrame();
+		frame.setLayout(new BorderLayout());
 		MancalaBoard b = new MancalaBoard(3);
-		MancalaView frame = new MancalaView(b);
+		MancalaView v = new MancalaView(0, 0, b);
+		frame.add(v, BorderLayout.CENTER);
+		frame.setSize(400, 400);
+		frame.setTitle("Mancala Game");
+		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
