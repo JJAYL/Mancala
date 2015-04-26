ceba653c9d0a0ab4170422a6ed4cabe211bfe852
Inventedforms <artifion@gmail.com>
2015-04-26 11:38:20 -0700

    Update Tester.java
    This might or might not work - it spawns a window with buttons 3 and 4, and clicking them should set the initial amount of stones to 3 or 4.
    I'm going to see if I can create the game only after one of the buttons are clicked.

--- Tester.java
+++ Tester.java
@@ -1,12 +1,39 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+
 import java.awt.*;
+import java.awt.event.*;
 
 public class Tester {
+	private static int init;
 	public static void main(String[] args)
 	{
+		JFrame select = new JFrame();
+		select.setLayout(new BorderLayout());
+		JButton three = new JButton("3");
+		three.addActionListener(new ActionListener()
+		{
+			 public void actionPerformed(ActionEvent a)
+			{
+				init = 3;
+				select.dispatchEvent(new WindowEvent(select, WindowEvent.WINDOW_CLOSING));
+			}
+		});
+		JButton four = new JButton("4");
+		four.addActionListener(new ActionListener()
+		{
+			 public void actionPerformed(ActionEvent a)
+			{
+				init = 4;
+				select.dispatchEvent(new WindowEvent(select, WindowEvent.WINDOW_CLOSING));
+			}
+		});
+		select.add(three, BorderLayout.WEST);
+		select.add(four, BorderLayout.EAST);
+		select.pack();
+		select.setVisible(true);
 		JFrame frame = new JFrame();
 		frame.setLayout(new BorderLayout());
-		MancalaBoard b = new MancalaBoard(3);
+		MancalaBoard b = new MancalaBoard(init);
 		MancalaView v = new MancalaView(0, 0, b);
 		frame.add(v, BorderLayout.CENTER);
 		frame.setSize(400, 400);
